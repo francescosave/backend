7568020da1e0bc642d645ea8f4eb71569bde7a10
Francesco Ricci <francescosav.ricci@libero.it>
2024-05-15 06:10:44 +0200

    add route root

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ fastify.get('/credits', (request, reply) => {
     });
 });
  
+fastify.get('/', (request, reply) => {
+    reply.send({
+        message: {
+            Components : ['Angela','Chiara','Maria','Francesco'],
+            github : 'https://github.com/tomorrowdevs-projects/tech-jobs-fair-hackathon-bari-2024-team5/'
+
+        }
+    });
+});
+
 fastify.get('/hello-ws', { websocket: true }, (connection, req) => {
     connection.socket.on('message', message => {
         connection.socket.send('Hello Fastify WebSockets');
