a2ff13a835dd09100ada97f5d875e261f3cec3f1
Francesco Ricci <francescosav.ricci@libero.it>
2024-05-15 06:05:37 +0200

    add structure backend with fastify

---
+++ main.js
@@ -0,0 +1,27 @@
+const fastify = require('fastify')();
+fastify.register(require('@fastify/websocket'));
+
+fastify.get('/credits', (request, reply) => {
+    reply.send({
+        message: {
+            Components : ['Angela','Chiara','Maria','Francesco'],
+            github : 'https://github.com/tomorrowdevs-projects/tech-jobs-fair-hackathon-bari-2024-team5/',
+            stack : 'Html CSS js vanilla & fastify'
+
+        }
+    });
+});
+ 
+fastify.get('/hello-ws', { websocket: true }, (connection, req) => {
+    connection.socket.on('message', message => {
+        connection.socket.send('Hello Fastify WebSockets');
+    });
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+    if(err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at: ${address}`);
+});
